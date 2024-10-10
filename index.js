55254c755b29b1ad74e70c3fbbcf5543bc61b327
tjc234 <tjc234@nau.edu>
2024-10-10 14:13:17 -0700

    change pg import

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { Pool } from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 
 // initialize global variables
 const app = express();
